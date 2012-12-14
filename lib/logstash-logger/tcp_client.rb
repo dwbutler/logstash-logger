4467b335dbf52e55d66a1c57b000738639bfadab
David Butler <dwbutler@ucla.edu>
2012-12-14 14:05:52 -0800

    Removed some cruft

--- lib/logstash-logger/tcp_client.rb
+++ lib/logstash-logger/tcp_client.rb
@@ -1,71 +1,19 @@
 class LogStashLogger::TCPClient
-  # This implementation is shamelessly copied from https://github.com/logstash/logstash/blob/master/lib/logstash/outputs/tcp.rb
   def initialize(host, port)
     @host = host
     @port = port
     connect
-    @queue = Queue.new
   end
   
   def write(event)
-    #@queue << event
     begin
       connect unless @socket
-    
-      #event = @queue.pop
-      wire_event = "#{event.to_hash.to_json}\n"
-      @socket.write(wire_event)
-    rescue
-      @socket.close
       
       @socket.write("#{event.to_hash.to_json}\n")
     rescue => e
       warn e.message
       @socket && @socket.close rescue nil
       @socket = nil
-    end
-  end
-  
-  public
-  def run
-    @socket_thread ||= Thread.new do
-      loop do
-        begin
-          connect unless @socket
-        
-          event = @queue.pop
-          wire_event = "#{event.to_hash.to_json}\n"
-          @socket.write(wire_event)
-        rescue => e
-          @socket.close
-          @socket = nil
-        end
-      end
-    end
-  end
-
-  public
-  def receive(event)
-    return unless output?(event)
-
-    wire_event = event.to_hash.to_json + "\n"
-
-    if server?
-      @client_threads.each do |client_thread|
-        client_thread[:client].write(wire_event)
-      end
-
-      @client_threads.reject! {|t| !t.alive? }
-    else
-      begin
-        connect unless @client_socket
-        @client_socket.write(event.to_hash.to_json)
-        @client_socket.write("\n")
-      rescue => e
-        @logger.warn("tcp output exception", :host => @host, :port => @port,
-                     :exception => e, :backtrace => e.backtrace)
-        connect
-      end
     end
   end
   
