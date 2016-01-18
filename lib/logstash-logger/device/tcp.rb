7a28d90864e64acaa6851410fbea4f2b5ef20784
Craig Read <craigabread@gmail.com>
2016-01-18 11:28:40 +1100

    Making SSL context configurable.
    Also adding a deprecation warning for when the SSL context
    has not been provided.

--- lib/logstash-logger/device/tcp.rb
+++ lib/logstash-logger/device/tcp.rb
@@ -10,7 +10,7 @@ module LogStashLogger
 
         @ssl_certificate = opts[:ssl_certificate]
         @use_ssl = !!(@ssl_certificate || opts[:use_ssl] || opts[:ssl_enable])
-        @ssl_context = opts.fetch(:ssl_context, OpenSSL::SSL::SSLContext.new)
+        @ssl_context = opts.fetch(:ssl_context, nil)
       end
 
       def use_ssl?
@@ -38,9 +38,15 @@ module LogStashLogger
       def ssl_connect
         non_ssl_connect
         #openssl_cert = OpenSSL::X509::Certificate.new(::File.read(@ssl_certificate))
-        @io = OpenSSL::SSL::SSLSocket.new(@io, @ssl_context)
-        @io.connect
-        @io.post_connection_check(@host)
+        if @ssl_context
+          @io = OpenSSL::SSL::SSLSocket.new(@io, @ssl_context)
+          @io.connect
+          @io.post_connection_check(@host)
+        else
+          warn "[DEPRECATION] 'LogStashLogger::Device::Socket' should be instantiated with a SSL context for hostname verification."
+          @io = OpenSSL::SSL::SSLSocket.new(@io)
+          @io.connect
+        end
       end
     end
   end
