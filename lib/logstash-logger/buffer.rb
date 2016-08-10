833ac636945c62677e03c2a7915eb8f5b520750b
David Butler <dwbutler@ucla.edu>
2016-08-09 17:49:42 -0700

    Flush log messages when exiting
    The current implementation of message buffering puts messages into a queue, and flushes them every five seconds. If the program exits, any messages in the buffer will be lost.
    
    This fixes the issue by automatically flushing the buffer when the program exits. This is done by registering an `at_exit` block which does a final flush, which will wait for all messages to be flushed. If an exception occurs, it will be logged and ignored.
    
    This option is configurable with the new option `buffer_flush_at_exit`, which now defaults to `true`.

--- lib/logstash-logger/buffer.rb
+++ lib/logstash-logger/buffer.rb
@@ -95,8 +95,13 @@ module LogStashLogger
         :has_on_flush_error => self.class.method_defined?(:on_flush_error),
         :has_on_full_buffer_receive => self.class.method_defined?(:on_full_buffer_receive),
         :drop_messages_on_flush_error => options.fetch(:drop_messages_on_flush_error, false),
-        :drop_messages_on_full_buffer => options.fetch(:drop_messages_on_full_buffer, false)
+        :drop_messages_on_full_buffer => options.fetch(:drop_messages_on_full_buffer, false),
+        :flush_at_exit => options.fetch(:flush_at_exit, false)
       }
+
+      if @buffer_config[:flush_at_exit]
+        at_exit { buffer_flush(final: true) }
+      end
 
       reset_buffer
     end
