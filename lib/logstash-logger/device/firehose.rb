7661c0e147a60883e07fe103725a461aa468c9d7
Bin Lan <lanxx019@gmail.com>
2017-04-07 18:17:39 -0400

    Refactor

--- lib/logstash-logger/device/firehose.rb
+++ lib/logstash-logger/device/firehose.rb
@@ -1,63 +1,40 @@
-require 'aws-sdk'
 require 'logstash-logger/device/aws_stream'
 
 module LogStashLogger
   module Device
-    class Firehose < Connectable
-      include Device::AwsStream
-
-      DEFAULT_REGION = 'us-east-1'
-      DEFAULT_STREAM = 'logstash'
-      RECOVERABLE_ERROR_CODES = [
-        "ServiceUnavailable",
-        "InternalFailure",
-        "ServiceUnavailableException"
-      ]
-
-      attr_accessor :aws_region, :stream
+    class Firehose < AwsStream
 
       def initialize(opts)
         super
-        @access_key_id = opts[:aws_access_key_id] || ENV['AWS_ACCESS_KEY_ID']
-        @secret_access_key = opts[:aws_secret_access_key] || ENV['AWS_SECRET_ACCESS_KEY']
-        @aws_region = opts[:aws_region] || DEFAULT_REGION
-        @stream = opts[:stream] || DEFAULT_STREAM
+        @stream_class = ::Aws::Firehose::Client
+        @recoverable_error_codes = [
+          "ServiceUnavailable",
+          "InternalFailure",
+          "ServiceUnavailableException"
+        ].freeze
       end
 
-      def connect
-        @io = ::Aws::Firehose::Client.new(
-          region: @aws_region,
-          credentials: ::Aws::Credentials.new(@access_key_id, @secret_access_key)
-        )
+      def transform_message(message)
+        {
+          data: message
+        }
       end
 
-      def write_batch(messages, group = nil)
-        firehose_records = messages.map do |message|
-          {
-            data: message,
-          }
-        end
-
-        with_connection do
-          resp = @io.put_record_batch({
-            records: firehose_records,
-            delivery_stream_name: @stream
-          })
-
-          # Put any failed records back into the buffer
-          if resp.failed_put_count != 0
-            resp.request_responses.each_with_index do |record, index|
-              if RECOVERABLE_ERROR_CODES.include?(record.error_code)
-                log_warning("Failed to post record to firehose with error: #{record.error_code} #{record.error_message}")
-                log_warning("Retrying")
-                write(firehose_records[index][:data])
-              elsif !record.error_code.nil? && record.error_code != ''
-                log_error("Failed to post record to firehose with error: #{record.error_code} #{record.error_message}")
-              end
-            end
-          end
-        end
+      def put_records(records)
+        @io.put_record_batch({
+          records: records,
+          delivery_stream_name: @stream
+        })
       end
+
+      def is_successful_response(resp)
+        resp.failed_put_count == 0
+      end
+
+      def get_response_records(resp)
+        resp.request_responses
+      end
+
     end
   end
 end
--- lib/logstash-logger/device/kinesis.rb
+++ lib/logstash-logger/device/kinesis.rb
@@ -1,65 +1,42 @@
-require 'aws-sdk'
 require 'logstash-logger/device/aws_stream'
 
 module LogStashLogger
   module Device
-    class Kinesis < Connectable
-      include Device::AwsStream
-
-      DEFAULT_REGION = 'us-east-1'
-      DEFAULT_STREAM = 'logstash'
-      RECOVERABLE_ERROR_CODES = [
-        "ServiceUnavailable",
-        "Throttling",
-        "RequestExpired",
-        "ProvisionedThroughputExceededException"
-      ]
-
-      attr_accessor :aws_region, :stream
+    class Kinesis < AwsStream
 
       def initialize(opts)
         super
-        @access_key_id = opts[:aws_access_key_id] || ENV['AWS_ACCESS_KEY_ID']
-        @secret_access_key = opts[:aws_secret_access_key] || ENV['AWS_SECRET_ACCESS_KEY']
-        @aws_region = opts[:aws_region] || DEFAULT_REGION
-        @stream = opts[:stream] || DEFAULT_STREAM
+        @stream_class = ::Aws::Kinesis::Client
+        @recoverable_error_codes = [
+          "ServiceUnavailable",
+          "Throttling",
+          "RequestExpired",
+          "ProvisionedThroughputExceededException"
+        ].freeze
       end
 
-      def connect
-        @io = ::Aws::Kinesis::Client.new(
-          region: @aws_region,
-          credentials: ::Aws::Credentials.new(@access_key_id, @secret_access_key)
-        )
+      def transform_message(message)
+        {
+          data: message,
+          partition_key: SecureRandom.uuid
+        }
       end
 
-      def write_batch(messages, group = nil)
-        kinesis_records = messages.map do |message|
-          {
-            data: message,
-            partition_key: SecureRandom.uuid
-          }
-        end
-
-        with_connection do
-          resp = @io.put_records({
-            records: kinesis_records,
-            stream_name: @stream
-          })
-
-          # Put any failed records back into the buffer
-          if resp.failed_record_count != 0
-            resp.records.each_with_index do |record, index|
-              if RECOVERABLE_ERROR_CODES.include?(record.error_code)
-                log_warning("Failed to post record to kinesis with error: #{record.error_code} #{record.error_message}")
-                log_warning("Retrying")
-                write(kinesis_records[index][:data])
-              elsif !record.error_code.nil? && record.error_code != ''
-                log_error("Failed to post record to kinesis with error: #{record.error_code} #{record.error_message}")
-              end
-            end
-          end
-        end
+      def put_records(records)
+        @io.put_records({
+          records: records,
+          stream_name: @stream
+        })
       end
+
+      def is_successful_response(resp)
+        resp.failed_record_count == 0
+      end
+
+      def get_response_records(resp)
+        resp.records
+      end
+
     end
   end
 end
